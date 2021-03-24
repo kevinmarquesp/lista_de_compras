de1bad506b0137c734b4a636e7d2a83f95bdffba
Kevin <kevinmarqueskmp@gmail.com>
2021-03-24 19:13:53 -0300

    Estilo finalizado

---
+++ scripts/temp.js
@@ -0,0 +1,7 @@
+const pagina_inicial = document.getElementById( 'pagina_inicial')
+const adicionar_item = document.getElementById( 'adicionar_item')
+const editar_item = document.getElementById( 'editar_item')
+
+pagina_inicial.style.display = 'initial'
+adicionar_item.style.display = 'none'
+editar_item.style.display = 'none'
